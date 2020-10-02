76b8548693eea7adf95830e730a34613e10e2295
Crowdbotics <team@crowdbotics.com>
2020-10-02 15:19:39 +0000

    Update models

--- backend/sfsdf_20977/wsgi.py
+++ backend/sfsdf_20977/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sfsdf_20977.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sfsdf_20977.settings")
 
 application = get_wsgi_application()
